37766c208ec69fc91fe74776e3f0f35f663a2e26
Eleazar Meza <bairesdev_emeza@iseatz.com>
2018-08-21 23:51:47 -0400

    Updated parser spec

--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 RSpec.describe Rowling::Score do
   describe '.parse' do
-    it 'parses a game' do
-      game_file_path = fixture_path('sample_game.txt')
-      expected_parsed_game = YAML.load_file fixture_path('sample_game.parsed.yaml')
+    let(:game_file_path) { fixture_path('sample_game.txt') }
+    let(:expected_parsed_game) { YAML.load_file fixture_path('sample_game.parsed.yaml') }
 
-      expect(Rowling::Score.parse(game_file_path)).to eq expected_parsed_game
+    it 'parses a game' do
+      parsed_game = Rowling::Score.parse(game_file_path)
+
+      expect(parsed_game).to eq expected_parsed_game
     end
   end
 
