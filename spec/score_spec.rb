dacc3d087fb62c8c10e119d872425a8f8e573911
Eleazar Meza <bairesdev_emeza@iseatz.com>
2018-08-22 00:20:34 -0400

    Created score spec

--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -3,16 +3,23 @@ require 'spec_helper'
 RSpec.describe Rowling::Score do
   describe '.parse' do
     let(:game_file_path) { fixture_path('sample_game.txt') }
-    let(:expected_parsed_game) { YAML.load_file fixture_path('sample_game.parsed.yaml') }
+    let(:expected_game_hash) { YAML.load_file fixture_path('sample_game_hash.yaml') }
 
     it 'parses a game' do
-      parsed_game = Rowling::Score.parse(game_file_path)
+      game_hash = Rowling::Score.parse(game_file_path)
 
-      expect(parsed_game).to eq expected_parsed_game
+      expect(game_hash).to eq expected_game_hash
     end
   end
 
   describe '.score' do
-    it 'scores a game'
+    let(:game_hash) { YAML.load_file fixture_path('sample_game_hash.yaml') }
+    let(:expected_game_score) { YAML.load_file fixture_path('sample_game_score.yaml') }
+
+    it 'scores a game' do
+      game_score = Rowling::Score.score(game_hash)
+
+      expect(game_score).to eq expected_game_score
+    end
   end
 end
